b7e9b95126863ba5087df488073267943512114a
199287 <zz1122zz>
2017-07-18 18:30:29 +0800

    增加zookeeper功能，测试完成版

---
+++ src/main/java/org/creation/singlejob/manager/observerpool/ZooKeeperObserverPool.java
@@ -0,0 +1,75 @@
+package org.creation.singlejob.manager.observerpool;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.zookeeper.KeeperException.NodeExistsException;
+import org.apache.zookeeper.WatchedEvent;
+import org.creation.singlejob.manager.SingleJobManager;
+
+import com.alibaba.fastjson.JSON;
+
+/** 
+* @author 作者 LiuPeng E-mail: 
+* @version 创建时间：2017年7月18日 下午4:18:03 
+* 类说明 
+*/
+/**
+ * <p>Title: Module Information         </p>
+ * <p>Description: Function Description </p>
+ * <p>Copyright: Copyright (c) 2017     </p>
+ * <p>Company: ND Co., Ltd.       </p>
+ * <p>Create Time: 2017年7月18日           </p>
+ * @author LiuPeng
+ * <p>Update Time: 2017年7月18日               </p>
+ * <p>Updater:                          </p>
+ * <p>Update Comments:                  </p>
+ */
+public class ZooKeeperObserverPool extends LocalConcurrentHashMapObserverPool {
+
+    private static final String PATH_PRIFIX = "/SJObserverPool/";
+
+    CuratorFramework zooKeeperClient;
+
+    /**
+     * @param zooKeeperClient
+     */
+    public ZooKeeperObserverPool(CuratorFramework zooKeeperClient) {
+        this.zooKeeperClient = zooKeeperClient;
+    }
+
+    @Override
+    public boolean putMeIntoObserverPool(String key, final SingleJobManager g) {
+        final String path = PATH_PRIFIX.concat(key);
+        try {
+            try {
+                zooKeeperClient.create().creatingParentsIfNeeded().forPath(path, JSON.toJSONBytes(g));
+            } catch (NodeExistsException e) {
+                //节点已经存在，视为创建成功。
+            }
+            zooKeeperClient.getData().usingWatcher(new CuratorWatcher() {
+                @Override
+                public void process(WatchedEvent event) throws Exception {
+                    Object msg = JSON.parse(zooKeeperClient.getData().forPath(path));
+                    g.pizzaDeliverd(msg);
+                    synchronized (g) {
+                        g.notify();
+                    }
+                }
+            }).forPath(path);
+            return true;
+        } catch (Exception e) {
+            return super.putMeIntoObserverPool(key, g);
+        }
+    }
+
+    public void publish(String key, Object resp) throws Exception {
+        String path = PATH_PRIFIX.concat(key);
+        try {
+            zooKeeperClient.create().creatingParentsIfNeeded().forPath(path, JSON.toJSONBytes(resp));
+        } catch (NodeExistsException e) {
+            //节点已经存在，改作修改
+            zooKeeperClient.setData().forPath(path, JSON.toJSONBytes(resp));
+        }
+    }
+
+}
---
+++ src/main/java/org/creation/singlejob/manager/worker/ZooKeeperSameReturnManager.java
@@ -0,0 +1,27 @@
+package org.creation.singlejob.manager.worker;
+
+import org.apache.zookeeper.KeeperException;
+import org.creation.singlejob.manager.observerpool.ZooKeeperObserverPool;
+
+public class ZooKeeperSameReturnManager extends SameReturnManager {
+    
+    public ZooKeeperSameReturnManager(boolean readCacheIfExist) {
+        super(readCacheIfExist);
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void callObservers(String uniqueKey, Object resp) {
+        if(observerPool instanceof ZooKeeperObserverPool)
+        {
+            try {
+                ((ZooKeeperObserverPool)observerPool).publish(uniqueKey, resp);
+            } catch (KeeperException e) {
+                super.callObservers(uniqueKey, resp);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
